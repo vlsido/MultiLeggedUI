ac9f33f821c08c66a4387c218a5791a1b9fee05b
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-05-31 23:13:54 +0300

    feat: add navigation indicators

--- app/components/views/Navigation.tsx
+++ app/components/views/Navigation.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router";
 import { SearchIcon } from "../icons/SearchIcon";
 import { ShoppingCartIcon } from "../icons/ShoppingCartIcon";
 import { StoreIcon } from "../icons/StoreIcon";
@@ -9,13 +9,23 @@ function Navigation() {
 
   return (
     <div className="flex w-[100%] h-16 gap-2.5 bg-brown-400 justify-center items-center">
-      <div className="px-[15px] py-1.25 bg-gray-200/50 rounded-full">
-        <StoreIcon />
-      </div>
-      <div className="px-[15px] py-[5px] bg-gray-200/50 rounded-full">
-        <ShoppingCartIcon />
-      </div>
-      <div className="px-[15px] py-[5px] bg-gray-200/50 rounded-full">
+
+      <Link
+        to={{ pathname: "/" }}
+      >
+        <div className={"px-[15px] py-1.25 rounded-full " + (location.pathname === "/" ? "bg-gray-200" : "bg-gray-200/50")}>
+          <StoreIcon />
+        </div>
+      </Link>
+
+      <Link
+        to={{ pathname: "/cart" }}
+      >
+        <div className={"px-[15px] py-1.25 rounded-full " + (location.pathname === "/cart" ? "bg-gray-200" : "bg-gray-200/50")}>
+          <ShoppingCartIcon />
+        </div>
+      </Link>
+      <div className={"px-[15px] py-1.25 rounded-full " + (location.pathname === "/search" ? "bg-gray-200" : "bg-gray-200/50")}>
         <SearchIcon />
       </div>
     </div>

6ff1dfcb7b9c1ff803f99963b2af574363f55615
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-06-01 00:46:58 +0300

    feat: add 'Add to cart' button to store

--- app/components/views/Store.tsx
+++ app/components/views/Store.tsx
@@ -1,43 +1,69 @@
 import { animalsData } from "~/data/data";
 import { FiltersIcon } from "../icons/FiltersIcon";
+import TextButton from "../buttons/TextButton";
 
 function Store() {
 
   return (
     <div
+      data-testid="STORE.CONTAINER:VIEW"
       className="flex p-2.5 place-content-center overflow-y-auto"
     >
-      <div className="flex flex-col items-end max-w-[1000px] gap-2.5">
+      <div
+        data-testid="STORE.CONTAINER.BODY:VIEW"
+        className="flex flex-col items-end max-w-[1000px] gap-2.5">
         <button className="flex flex-row gap-1 text-black py-1 px-2 bg-yellow-100 rounded-full cursor-pointer drop-shadow-md">
           <FiltersIcon />
           Filters
         </button>
-        <div className="flex flex-col gap-2.5">
+        <div
+          data-testid="STORE.CONTAINER.BODY.ANIMAL_DATA:LIST"
+          className="flex flex-col gap-2.5">
           {animalsData.map((
             data, index
           ) => {
             return (
               <div
                 key={index}
+                data-testid={`STORE.CONTAINER.BODY.ANIMAL_DATA:ITEM-${index}`}
                 className="bg-gray-100 rounded-xl">
                 <div className="px-6 py-2 text-black font-bold">
                   {data.group}
                 </div>
-                <div className="flex px-4 py-4 gap-4 flex-wrap justify-center">
-                  {data.animals.map((animal) => {
+                <div
+                  data-testid={"STORE.CONTAINER.BODY.ANIMAL_DATA.ANIMAL:LIST"}
+                  className="flex px-4 py-4 gap-4 flex-wrap justify-center">
+                  {data.animals.map((
+                    animal, index
+                  ) => {
                     return (
-                      <div key={animal.name} className="flex flex-col max-w-[400px] bg-gray-400 p-5 rounded-xl">
+                      <div
+                        key={animal.name}
+                        data-testid={`STORE.CONTAINER.BODY.ANIMAL_DATA.ANIMAL:ITEM-${index}`}
+                        className="flex flex-col max-w-[400px] bg-gray-400 p-5 rounded-xl">
                         <div className="p-5 bg-gray-500 rounded-xl">
                           <p className="text-black">{animal.name}</p>
                           <img src={animal.imageUrl} />
                         </div>
-                        <div className="text-black">
+                        <div
+                          data-testid={`STORE.CONTAINER.BODY.ANIMAL_DATA.ANIMAL.ITEM-${index}.INFO:VIEW`}
+                          className="text-black">
                           <p>Names: {animal.names}</p>
                           <p>Origin: {animal.origin}</p>
                           <p>Size: {animal.size}</p>
                           <p>Humidity: {animal.humidity}</p>
                           <p>Environment temp: {animal.environment}</p>
                           <p>Additional info: {animal.info}</p>
+                          <div className="flex flex-col w-full my-2 items-center text-[20px]">
+                            {animal.ppu}$/10pcs
+                            <TextButton
+                              testId={`STORE.CONTAINER.BODY.ANIMAL_DATA.ANIMAL.ITEM-${index}.INFO.ADD_TO_CART:BUTTON`}
+                              ariaLabel={`Add ${animal.name} to cart`}
+                              text="Add to cart"
+                              containerClassName="px-10 py-4 w-full rounded-full bg-black-500 text-white cursor-pointer"
+                              textClassName="text-[20px]"
+                              onPress={() => { }} />
+                          </div>
                         </div>
                       </div>
                     );

79fae6243aa75397e435a55743a8c398b44e619f
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-06-02 02:38:17 +0300

    style: adjust navbar for mobiles

--- app/components/views/Navigation.tsx
+++ app/components/views/Navigation.tsx
@@ -9,27 +9,30 @@ function Navigation() {
 
 
   return (
-    <div className="flex w-[100%] h-16 gap-2.5 bg-brown-400 justify-center items-center"
-    >
-      <Link
-        to={{ pathname: "/" }}
+    <>
+      <div className="flex fixed right-0 left-0 bottom-0 w-[100%] h-16 gap-2.5 bg-brown-400 justify-center items-center"
       >
-        <div className={"px-[15px] py-1.25 rounded-full " + (location.pathname === "/" ? "bg-gray-200" : "bg-gray-200/50")}>
-          <StoreIcon />
-        </div>
-      </Link>
+        <Link
+          to={{ pathname: "/" }}
+        >
+          <div className={"px-[15px] py-1.25 rounded-full " + (location.pathname === "/" ? "bg-gray-200" : "bg-gray-200/50")}>
+            <StoreIcon />
+          </div>
+        </Link>
 
-      <Link
-        to={{ pathname: "/cart" }}
-      >
-        <div className={"px-[15px] py-1.25 rounded-full " + (location.pathname === "/cart" ? "bg-gray-200" : "bg-gray-200/50")}>
-          <ShoppingCartIcon />
+        <Link
+          to={{ pathname: "/cart" }}
+        >
+          <div className={"px-[15px] py-1.25 rounded-full " + (location.pathname === "/cart" ? "bg-gray-200" : "bg-gray-200/50")}>
+            <ShoppingCartIcon />
+          </div>
+        </Link>
+        <div className={"px-[15px] py-1.25 rounded-full " + (location.pathname === "/search" ? "bg-gray-200" : "bg-gray-200/50")}>
+          <SearchIcon />
         </div>
-      </Link>
-      <div className={"px-[15px] py-1.25 rounded-full " + (location.pathname === "/search" ? "bg-gray-200" : "bg-gray-200/50")}>
-        <SearchIcon />
       </div>
-    </div >
+      <div className="flex w-[100%] h-16" />
+    </>
   );
 }
 

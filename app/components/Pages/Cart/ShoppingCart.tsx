66f54286e54c9a09072880b2d6cf5e65160ab8c6
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-06-14 16:12:02 +0300

    feat: add back/checkout button; style: responsive design

--- app/components/Pages/Cart/ShoppingCart.tsx
+++ app/components/Pages/Cart/ShoppingCart.tsx
@@ -1,5 +1,7 @@
 import { useCallback } from "react";
+import { useNavigate } from "react-router";
 import { TrashIcon } from "~/components/Icons/TrashIcon";
+import TextButton from "~/components/UI/buttons/TextButton";
 import Footer from "~/components/UI/Footer";
 import Notification from "~/components/UI/Notification";
 import QuantitySelector from "~/components/UI/QuantitySelector";
@@ -11,6 +13,8 @@ function ShoppingCart() {
   const species = useAppSelector((state) => state.species.species);
 
   const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
 
   function findPriceByPackId(id: number): number {
     for (const category of species) {
@@ -43,12 +47,26 @@ function ShoppingCart() {
     []
   );
 
+  const handleGoBack = useCallback(
+    () => {
+      return navigate("/");
+    },
+    []
+  );
+
+  const handleGoCheckout = useCallback(
+    () => {
+      return navigate("/checkout");
+    },
+    []
+  );
+
   return (
     <div
       data-testid="SHOPPING_CART.CONTAINER:VIEW"
       className="flex flex-1 flex-col justify-between overflow-y-auto"
     >
-      <div className="flex flex-1 p-2.5 justify-center items-center flex-col gap-2.5">
+      <div className="flex flex-1 p-5 justify-center items-center flex-col gap-2.5">
         {cart.length === 0 ? (
           <Notification
             testId="SHOPPING_CART.CONTAINER.NOTIFICATION:VIEW"
@@ -56,42 +74,66 @@ function ShoppingCart() {
             severity="INFO"
           />
         ) : (
-          <div className="flex flex-col max-w-[1000px] w-[100%] bg-white">
-            {cart.map((item) => {
+          <>
+            <p className="flex md:hidden text-[20px] self-start font-bold">CART</p>
+            <div className="flex flex-col max-w-[1000px] w-[100%] bg-white">
+              {cart.map((item) => {
 
-              const price = findPriceByPackId(item.packId) / 100;
+                const price = findPriceByPackId(item.packId) / 100;
 
-              return (
-                <div
-                  key={item.packId}
-                  className="flex justify-between items-center px-2.5 py-4 gap-2.5 border-1 text-black">
-                  <div className="flex-1">
-                    <img src={item.imageUrl} className="h-32 w-32 rounded-xl object-contain" />
+                return (
+                  <div
+                    key={item.packId}
+                    className="flex flex-col md:flex-row justify-between items-center px-10 md:px-2.5 py-4 gap-2.5 border-1 text-black">
+                    <button
+                      className="flex w-[100%] justify-end md:hidden cursor-pointer"
+                      onPointerUp={() => removeItem(item.packId)}>
+                      <TrashIcon />
+                    </button>
+                    <div className="flex-1">
+                      <img src={item.imageUrl} className="h-32 w-32 rounded-xl object-contain" />
+                    </div>
+                    <p className="flex-1 underline text-center">
+                      {item.name} x {item.units}
+                    </p>
+                    <div className="flex md:flex-1 w-[100%] justify-between">
+                      <p className="md:hidden">Price:</p>
+                      <p className="md:flex-1 text-center">
+                        ${price}
+                      </p>
+                    </div>
+                    <div className="flex md:flex-1 w-[100%] justify-between items-center">
+                      <p className="md:hidden">Quantity:</p>
+                      <QuantitySelector
+                        initialQuantity={item.quantity}
+                        onAdd={() => addQuantity(item.packId)}
+                        onRemove={() => removeQuantity(item.packId)}
+                      />
+                    </div>
+                    <div className="flex md:flex-1 w-[100%] justify-between">
+                      <p className="md:hidden">Sub-total:</p>
+                      <p className="md:flex-1 text-center">
+                        ${(price * item.quantity).toFixed(2)}
+                      </p>
+                    </div>
+                    <button
+                      className="hidden md:flex cursor-pointer"
+                      onPointerUp={() => removeItem(item.packId)}>
+                      <TrashIcon />
+                    </button>
                   </div>
-                  <p className="flex-1 underline text-center">
-                    {item.name}
-                  </p>
-                  <p className="flex-1 text-center">
-                    ${price}
-                  </p>
-                  <QuantitySelector
-                    initialQuantity={item.quantity}
-                    onAdd={() => addQuantity(item.packId)}
-                    onRemove={() => removeQuantity(item.packId)}
-                  />
-                  <p className="flex-1 text-center">
-                    ${(price * item.quantity).toFixed(2)}
-                  </p>
-                  <button
-                    className="cursor-pointer"
-                    onPointerUp={() => removeItem(item.packId)}>
-                    <TrashIcon />
-                  </button>
-                </div>
-              );
-            })}
-          </div>
+                );
+              })}
+            </div>
+          </>
         )}
+        <TextButton
+          text={cart.length === 0 ? "Back to store" : "Proceed to checkout"}
+          ariaLabel={cart.length === 0 ? "Go back" : "Go checkout"}
+          containerClassName="bg-yellow-100 w-full max-w-[250px] p-2.5 rounded-full drop-shadow-md cursor-pointer"
+          textClassName="text-black-500"
+          onPress={cart.length === 0 ? handleGoBack : handleGoCheckout}
+        />
       </div>
       <Footer />
     </div>

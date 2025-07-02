9e6cf5daa3624a21d17e16c556574a647caf34f9
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-07-03 01:40:26 +0300

    refactor: mock data

--- app/data/data.ts
+++ app/data/data.ts
@@ -8,10 +8,14 @@ export const categoriesAnimalsData: CategoryAnimals[] = [
         id: 1,
         imageUrl: "porcellio_laevis_1.png",
         name: "Porcellio Laevis 'Milkback'",
-        description: "Origin: around globe\n Size: 2 - 3 cm\nHumidity: 60 %\nEnvironment temp: 18 - 26 C\nAdditional info: Large isopod, very popular and recognizable because of their unique coloring. Very sturdy and easy to keep and breed, perfect species for a beginner. Very forgiving when it comes to temperature changes and moisture fluctuations.",
+        origin: "Globe",
+        size: "2-3 cm",
+        humidity: "60%",
+        temperature: "18-24",
+        description: "Large isopod, very popular and recognizable because of their unique coloring. Very sturdy and easy to keep and breed, perfect species for a beginner. Very forgiving when it comes to temperature changes and moisture fluctuations.",
         units: 0,
         animalPrices: [
-          { id: 1, min_quantity: 8, max_quantity: 20, cents_per_unit: 599 }
+          { id: 1, minQuantity: 8, maxQuantity: 20, centsPerUnit: 599 }
         ],
         form: "animals"
       },
@@ -24,10 +28,14 @@ export const categoriesAnimalsData: CategoryAnimals[] = [
         id: 2,
         imageUrl: "medauroidea_extradentata_1.png",
         name: "Medauroidea extradentata 'Annam Walking Stick'",
-        description: "Origin: around globe\nSize: 2 - 3 cm\nHumidity: 60 %\nEnvironment temp: 18 - 26 C\nAdditional info: Large isopod, very popular and recognizable because of their unique coloring.Very sturdy and easy to keep and breed, perfect species for a beginner.Very forgiving when it comes to temperature changes and moisture fluctuations.",
+        origin: "Globe",
+        size: "2-3 cm",
+        humidity: "60%",
+        temperature: "18-24",
+        description: "Large isopod, very popular and recognizable because of their unique coloring.Very sturdy and easy to keep and breed, perfect species for a beginner. Very forgiving when it comes to temperature changes and moisture fluctuations.",
         units: 0,
         animalPrices: [
-          { id: 1, min_quantity: 8, max_quantity: 20, cents_per_unit: 599 }
+          { id: 1, minQuantity: 8, maxQuantity: 20, centsPerUnit: 599 }
         ],
         form: "eggs"
       },

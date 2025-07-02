8d98c595698ecaa2b123165031aca39c4645ea7b
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-07-03 02:57:47 +0300

    chore: remove stylistic rules from eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import stylistic from "@stylistic/eslint-plugin";
 import react from "eslint-plugin-react";
 import pluginQuery from "@tanstack/eslint-plugin-query";
+import eslintConfigPrettierFlat from "eslint-config-prettier/flat";
 
 export default [
   ...pluginQuery.configs["flat/recommended"],
   ...tseslint.configs.recommended,
+  eslintConfigPrettierFlat,
   {
     files: ["**/*.{jsx,ts,tsx}"],
     plugins: {
       react,
-      "@stylistic": stylistic,
     },
     languageOptions: {
       parserOptions: {
@@ -22,17 +22,6 @@ export default [
       },
     },
     rules: {
-      "@stylistic/quotes": ["warn", "double"],
-      "@stylistic/array-bracket-newline": ["warn", { minItems: 1 }],
-      "@stylistic/array-element-newline": ["warn", "always"],
-      "@stylistic/arrow-parens": ["warn", "always"],
-      "@stylistic/comma-spacing": ["warn", { before: false, after: true }],
-      "@stylistic/dot-location": ["warn", "object"],
-      "@stylistic/line-comment-position": ["warn", "above"],
-      "@stylistic/spaced-comment": ["warn", "always"],
-      "@stylistic/no-mixed-operators": ["warn", { allowSamePrecedence: false }],
-      "@stylistic/function-call-argument-newline": ["warn", "always"],
-      "@stylistic/function-paren-newline": ["warn", { minItems: 2 }],
       "prefer-const": "off",
       "no-async-promise-executor": ["error"],
       "no-await-in-loop": ["error"],

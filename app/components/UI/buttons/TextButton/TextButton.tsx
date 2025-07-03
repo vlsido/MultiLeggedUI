97352100137dbc3e906263102bdf1b6d309ef6ec
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-07-03 20:25:09 +0300

    refactor: move TextButton to separate folder

---
+++ app/components/UI/buttons/TextButton/TextButton.tsx
@@ -0,0 +1,33 @@
+import type { ReactElement } from "react";
+
+interface TextButtonProps {
+  text: string;
+  onPress: () => void;
+  ariaLabel: string;
+  containerClassName?: string;
+  textClassName?: string;
+  leftSideIcon?: ReactElement;
+  disabled?: boolean;
+}
+
+function TextButton(props: TextButtonProps) {
+  return (
+    <button
+      className={
+        props.containerClassName
+          ? props.containerClassName
+          : "flex flex-row items-center px-[16px] py-[10px] gap-[8px] bg-black rounded-[4px] cursor-pointer"
+      }
+      aria-label={props.ariaLabel}
+      disabled={props.disabled}
+      onPointerUp={props.onPress}
+    >
+      {props.leftSideIcon}
+      <p className={props.textClassName ? props.textClassName : "text-[14px]"}>
+        {props.text}
+      </p>
+    </button>
+  );
+}
+
+export default TextButton;

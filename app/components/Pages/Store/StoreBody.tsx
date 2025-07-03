11787c38e55d554da4628b5944a9204b3d755353
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-07-03 05:01:30 +0300

    style: add hover animation to categories

--- app/components/Pages/Store/StoreBody.tsx
+++ app/components/Pages/Store/StoreBody.tsx
@@ -8,6 +8,7 @@ import WorldIcon from "~/components/Icons/WorldIcon";
 import HumidityIcon from "~/components/Icons/HumidityIcon";
 import TemperatureIcon from "~/components/Icons/TemperatureIcon";
 import PurchaseView from "./PurchaseSection/PurchaseView";
+import { motion } from "motion/react";
 
 function StoreBody() {
   const categoriesAnimals = useAppSelector(
@@ -20,40 +21,48 @@ function StoreBody() {
     return (
       <div className="flex flex-1 flex-col justify-between overflow-y-auto">
         <div className="flex flex-1 justify-center items-center md:items-start p-2.5">
-          <div className="flex flex-col p-2.5 max-w-[1000px] gap-2.5 text-black bg-gray-100 rounded-xl ">
+          <div className="flex flex-col p-2.5 max-w-[1000px] gap-2.5 text-black md:mt-[10%] bg-gray-100 rounded-xl">
             <h1 className="text-[20px] font-bold">Categories</h1>
             <div className="flex flex-row flex-wrap justify-center gap-2.5 p-4">
-              <button
+              <motion.button
                 className="flex flex-col items-center cursor-pointer"
                 onPointerUp={() => setCategory("ALL")}
+                whileHover={{
+                  scale: 1.2,
+                  transition: { duration: 0.5 },
+                }}
               >
                 <div className="px-6 font-bold">
                   <h3>ALL</h3>
                 </div>
-                <div className="flex gap-3 p-5 bg-gray-500 rounded-xl items-center">
+                <div className="flex gap-3 p-5 bg-gray-500 ring-white ring-1 rounded-xl items-center">
                   <img
-                    className="h-32 w-32 rounded-full object-cover hover:scale-125"
+                    className="h-32 w-32 rounded-full object-cover"
                     src={"/all_categories.png"}
                   />
                 </div>
-              </button>
+              </motion.button>
               {categoriesAnimals.map((categoryAnimals, index) => {
                 return (
-                  <button
+                  <motion.button
                     key={index}
                     className="flex flex-col items-center cursor-pointer"
+                    whileHover={{
+                      scale: 1.2,
+                      transition: { duration: 0.5 },
+                    }}
                     onPointerUp={() => setCategory(categoryAnimals.category)}
                   >
-                    <div className="px-6  font-bold">
+                    <div className="px-6 font-bold">
                       <h3>{categoryAnimals.category.toUpperCase()}</h3>
                     </div>
-                    <div className="flex gap-3 p-5 bg-gray-500 rounded-xl items-center">
+                    <div className="flex gap-3 p-5 bg-gray-500 ring-white ring-1 rounded-xl items-center">
                       <img
-                        className="h-32 w-32 rounded-full object-cover hover:scale-125"
+                        className="h-32 w-32 rounded-full object-cover"
                         src={categoryAnimals.animals[0].imageUrl}
                       />
                     </div>
-                  </button>
+                  </motion.button>
                 );
               })}
             </div>

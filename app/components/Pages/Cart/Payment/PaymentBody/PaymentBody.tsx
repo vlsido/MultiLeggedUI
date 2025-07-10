4abd24f0dc0c69fbde6a6119a9defba94ba29ff9
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-07-11 02:15:52 +0300

    feat: add stripe to payment page

--- app/components/Pages/Cart/Payment/PaymentBody/PaymentBody.tsx
+++ app/components/Pages/Cart/Payment/PaymentBody/PaymentBody.tsx
@@ -1,5 +1,23 @@
+import { CheckoutProvider, PaymentElement } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51RjTKDPI2Lgb9onPFEhhdBOAbZWotOiwdhSuvJADkva1apcsQSbkpaFHeZx9hDbIdt0Ya5kYZeFnBYVSceC27Ygj00IN3wZ5Kd",
+);
+
+async function fetchClientSecret() {
+  return fetch("/create-checkout-session", { method: "POST" })
+    .then((response) => response.json())
+    .then((json) => json.checkoutSessionClientSecret);
+}
+
 function PaymentBody() {
-  return null;
+  return (
+    <CheckoutProvider stripe={stripePromise} options={{ fetchClientSecret }}>
+      <PaymentElement />
+      <button>Submit</button>
+    </CheckoutProvider>
+  );
 }
 
 export default PaymentBody;

744684552d7a73c4c3fddac0c6dbc7f63640a881
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-07-13 00:07:42 +0300

    style: make error message prettier

--- app/components/Helpers/UserMessageHelper/UserMessageCard.tsx
+++ app/components/Helpers/UserMessageHelper/UserMessageCard.tsx
@@ -1,4 +1,5 @@
 import { motion } from "motion/react";
+import { CrossedLineIcon } from "~/components/Icons/CrossedLineIcon";
 
 export interface UserMessageCardProps {
   text: string;
@@ -7,6 +8,7 @@ export interface UserMessageCardProps {
    * milliseconds
    */
   ms: number;
+  onClose: () => void;
 }
 
 function UserMessageCard(props: UserMessageCardProps) {
@@ -26,15 +28,35 @@ function UserMessageCard(props: UserMessageCardProps) {
     case "ERROR":
       return (
         <motion.div
-          className="absolute bg-white right-[50px] top-[100px] mx-auto self-center text-black max-w-[200px] min-w-[100px] border-black border-1 drop-shadow-xl z-1"
+          className="absolute bg-red-800 right-[50px] top-[100px] mx-auto self-center rounded-md min-w-[100px] border-black border-1 drop-shadow-xl z-1 overflow-hidden"
           initial={{ opacity: 0, x: 0 }}
           animate={{ opacity: 1, x: -50 }}
           exit={{ opacity: 0, x: 50 }}
           transition={{ duration: 0.25 }}
         >
-          <p className="text-center p-2.5">{props.text}</p>
+          <div className="flex gap-4 py-2.5 justify-center items-center">
+            <div className="flex justify-center items-center">
+              <div className="flex rotate-45 p-[1px] mx-2.5 scale-150 rounded-full border-1 border-white">
+                <CrossedLineIcon strokeWidth={2} />
+              </div>
+              <div className="flex flex-col">
+                <p className="text-center text-white text-[16px] font-bold">
+                  An error has occured
+                </p>
+                <p className="text-center text-white text-[14px]">
+                  {props.text}
+                </p>
+              </div>
+            </div>
+            <button
+              className="rotate-45 mx-2.5 scale-200 hover:opacity-50"
+              onPointerUp={props.onClose}
+            >
+              <CrossedLineIcon strokeWidth={1} />
+            </button>
+          </div>
           <motion.div
-            className="bg-red-800 w-full h-5"
+            className="bg-white/35 w-full h-1"
             animate={{ width: 0 }}
             transition={{ duration: props.ms / 1000 }}
           />
--- app/components/Helpers/UserMessageHelper/UserMessageHelper.tsx
+++ app/components/Helpers/UserMessageHelper/UserMessageHelper.tsx
@@ -1,10 +1,11 @@
 import { AnimatePresence } from "motion/react";
-import UserMessageCard, { type UserMessageCardProps } from "./UserMessageCard";
-import { useEffect, useRef, useState } from "react";
+import UserMessageCard from "./UserMessageCard";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { userMessageManager } from "~/managers/userMessageManager";
+import type { UserMessage } from "~/types/common";
 
 function UserMessageHelper() {
-  const [message, setMessage] = useState<UserMessageCardProps | null>(null);
+  const [message, setMessage] = useState<UserMessage | null>(null);
 
   const timeoutId = useRef<NodeJS.Timeout | undefined>(undefined);
 
@@ -24,6 +25,10 @@ function UserMessageHelper() {
     };
   }, []);
 
+  const handleOnClose = useCallback(() => {
+    setMessage(null);
+  }, []);
+
   return (
     <AnimatePresence>
       {message != null && (
@@ -31,6 +36,7 @@ function UserMessageHelper() {
           text={message.text}
           type={message.type}
           ms={message.ms}
+          onClose={handleOnClose}
         />
       )}
     </AnimatePresence>
